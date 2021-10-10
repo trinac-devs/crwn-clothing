bfb3803f6691df73cf9b961d06cb5ec436309e64
traxx10 <codetraxx10@gmail.com>
2021-10-10 12:10:41 +0100

    modified menu items to navigate to HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
 import './App.css';
+import React from 'react';
 import Homepage from './pages/homepage.component';
+import { Route, Switch } from 'react-router';
+
+const HatsPage = (props) => {
+  console.log(props, 'props');
+  return (
+    <div>
+      <h1>Hats Page</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
