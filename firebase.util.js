ebb53b5105c1dd248eba97f5416739ffcc542e3d
traxx10 <codetraxx10@gmail.com>
2021-10-20 05:57:45 +0100

    minor change

---
+++ firebase.util.js
@@ -0,0 +1,34 @@
+// Import the functions you need from the SDKs you need
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+import { getAnalytics } from 'firebase/analytics';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyDXFBd9qbjvzwBTScfw673M3VWq7KdlDMA',
+  authDomain: 'crwn-clothing-3ce45.firebaseapp.com',
+  projectId: 'crwn-clothing-3ce45',
+  storageBucket: 'crwn-clothing-3ce45.appspot.com',
+  messagingSenderId: '725214747657',
+  appId: '1:725214747657:web:1f5cccd6bb4e8144ee6cd8',
+  measurementId: 'G-D107R9FSQ1',
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+
+export const signinWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import './App.css';
 import React from 'react';
-import Homepage from './pages/homepage.component';
-import { Route, Switch } from 'react-router';
+import { Switch, Route } from 'react-router-dom';
 
-const HatsPage = (props) => {
-  console.log(props, 'props');
-  return (
-    <div>
-      <h1>Hats Page</h1>
-    </div>
-  );
-};
+import './App.css';
+
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
-        <Route exact path="/" component={Homepage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
